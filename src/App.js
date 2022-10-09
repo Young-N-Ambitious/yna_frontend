197c57a3c842d8156427f154b9da23e872b99fbb
Anuj Verma <anujverma123ok@gmail.com>
2022-10-09 13:35:56 +0530

    Routes Instance Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import {HomePage, AboutUs, ContactUs, JoinUs, Members, Login, NotFound} from './pages'
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<HomePage/>} />
-        <Route exact path='/about-us' element={<AboutUs/>} />
+        {['aboutus', 'about-us', 'AboutUS','AboutUs','ABOUTUS','about','ABOUT'].map(path => <Route path={path} element={<AboutUs/>} />)}
+        {['contactus', 'contact-us', 'ContactUS','ContactUs','CONTACTUS','contact','CONTACT'].map(path => <Route path={path} element={<ContactUs/>} />)}
+        {['joinus', 'join-us', 'JOinUS','JoinUs','JOINUS','join','JOIN'].map(path => <Route path={path} element={<JoinUs/>} />)}
         <Route exact path='/members' element={<Members/>} />
-        <Route exact path='/contact-us' element={<ContactUs/>} />
-        <Route exact path='/join-us' element={<JoinUs/>} />
         <Route exact path='/login' element={<Login/>} />
 
         {/* The Below Route is for 404 #PageNotFound, Make sure to Place this route at the end of all Route [if want to add other Route, add Route above 404 Route] */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-
-    </BrowserRouter>
 
   );
 }
