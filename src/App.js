5fd85d2066f3736921da4e677b780196007b1ff8
Sincerely Brittany <sincerelybr@gmail.com>
2022-10-09 22:38:27 -0400

    bug - remove maps from routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Footer from "./pages/globalComponents/footer/Footer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import {
   HomePage,
   AboutUs,
@@ -16,47 +16,17 @@ function App() {
     <div className="">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        {[
-          "aboutus",
-          "about-us",
-          "AboutUS",
-          "AboutUs",
-          "ABOUTUS",
-          "about",
-          "ABOUT",
-        ].map((path) => (
-          <Route path={path} element={<AboutUs />} />
-        ))}
-        {[
-          "contactus",
-          "contact-us",
-          "ContactUS",
-          "ContactUs",
-          "CONTACTUS",
-          "contact",
-          "CONTACT",
-        ].map((path) => (
-          <Route path={path} element={<ContactUs />} />
-        ))}
-        {[
-          "joinus",
-          "join-us",
-          "JOinUS",
-          "JoinUs",
-          "JOINUS",
-          "join",
-          "JOIN",
-        ].map((path) => (
-          <Route path={path} element={<JoinUs />} />
-        ))}
+        <Route exact path="/about-us" element={<AboutUs />} />
+        <Route exact path="/contact-us" element={<ContactUs />} />
+        <Route exact path="/join-us" element={<JoinUs />} />
         <Route exact path="/members" element={<Members />} />
         <Route exact path="/login" element={<Login />} />
         {/* The Below Route is for 404 #PageNotFound, Make sure to Place this route at the end of all Route [if want to add other Route, add Route above 404 Route] */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-       <header className="flex justify-center items-center h-screen">
-         <p className="text-blue-500">tailwind added</p>
-        </header>
+      <header className="flex justify-center items-center h-screen">
+        <p className="text-blue-500">tailwind added</p>
+      </header>
       <Footer />
     </div>
   );
