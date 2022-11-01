7ed25015f1f407e63f526519db0cf9df7673924f
marlovigor <37669615+marlovigor@users.noreply.github.com>
2022-10-31 20:18:02 -0400

    typecript added (#31)
    Co-authored-by: Marlo Jones <marlojones@pop-os.localdomain>
    Co-authored-by: sydnee510 <sbsydnee161416@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Footer from "./pages/globalComponents/footer/Footer";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import {
   HomePage,
   AboutUs,
@@ -14,16 +14,18 @@ import {
 function App() {
   return (
     <div className="">
+      <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/about-us" element={<AboutUs />} />
-        <Route exact path="/contact-us" element={<ContactUs />} />
-        <Route exact path="/join-us" element={<JoinUs />} />
-        <Route exact path="/members" element={<Members />} />
-        <Route exact path="/login" element={<Login />} />
+        <Route  path="/" element={<HomePage />} />
+        <Route  path="/about-us" element={<AboutUs />} />
+        <Route  path="/contact-us" element={<ContactUs />} />
+        <Route  path="/join-us" element={<JoinUs />} />
+        <Route  path="/members" element={<Members />} />
+        <Route  path="/login" element={<Login />} />
         {/* The Below Route is for 404 #PageNotFound, Make sure to Place this route at the end of all Route [if want to add other Route, add Route above 404 Route] */}
         <Route path="*" element={<NotFound />} />
       </Routes>
+      </BrowserRouter>
       <header className="flex justify-center items-center h-screen">
         <p className="text-blue-500">tailwind added</p>
       </header>
