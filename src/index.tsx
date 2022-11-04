2f7c63ab7a8c8d27187cd2632c26fcf68e2b4ed3
sydnee510 <sbsydnee161416@gmail.com>
2022-11-04 08:40:33 -0700

    Update index.tsx -fix browser error

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomePage from "./pages/homePage/HomePage";
-import AboutUs from "./pages/aboutUs/AboutUs";
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import './index.css';
+// import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root"),
 );
