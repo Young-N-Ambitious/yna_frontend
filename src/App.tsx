902597813821a3c5e4771411b9fbc2f3d095e2f0
Danielle Torres <dctorres05@gmail.com>
2022-11-04 10:15:09 -0400

    remove browserrouter from routing config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Footer from "./pages/globalComponents/footer/Footer";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import {
   HomePage,
   AboutUs,
@@ -14,7 +14,6 @@ import {
 function App() {
   return (
     <div className="">
-      <BrowserRouter>
       <Routes>
         <Route  path="/" element={<HomePage />} />
         <Route  path="/about-us" element={<AboutUs />} />
@@ -25,7 +24,6 @@ function App() {
         {/* The Below Route is for 404 #PageNotFound, Make sure to Place this route at the end of all Route [if want to add other Route, add Route above 404 Route] */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-      </BrowserRouter>
       <header className="flex justify-center items-center h-screen">
         <p className="text-blue-500">tailwind added</p>
       </header>
