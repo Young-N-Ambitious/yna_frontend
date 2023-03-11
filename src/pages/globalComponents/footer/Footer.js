1a2d4a44034eda4328c095c4860d4243dcd1d301
carlos luevano <clue@pop-os.localdomain>
2023-03-10 22:46:58 -0600

    re styled footer using styled components

--- src/pages/globalComponents/footer/Footer.js
+++ src/pages/globalComponents/footer/Footer.js
@@ -1,35 +1,27 @@
 import React from "react";
 import twitter from "../../../assets/twitter.png";
 import linkedin from "../../../assets/Linkedin.png";
+import { FooterStyle } from "./Footer.style.js";
 
 const Footer = () => {
-  return (
-    <footer class="py-10 bg-[#ffd644] md:flex md:items-center md:justify-between md:p-6">
-      <span class="text-lg font-bold text-black sm:text-center">
-        © 2022 YOUNG N AMBITIOUS 2022
-      </span>
-      <ul class="flex flex-wrap items-center  text-sm space-x-5 text-gray-500 ">
-        <li>
-          <a
-            href="https://twitter.com/YNA_Squad"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img class="w-27.62 h-17.42 " src={twitter} alt="Twitter" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/company/yna-consult-group/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img class="w-29.52 h-22.3 " src={linkedin} alt="LinkedIn" />
-          </a>
-        </li>
-      </ul>
-    </footer>
-  );
+    return (
+        <FooterStyle>
+            <footer>
+                <div className="footercontainer">
+                    <span>© 2022 YOUNG N AMBITIOUS 2022</span>
+                    <div className="links">
+                        <a className="linkspace" href="https://twitter.com/YNA_Squad" target="_blank" rel="noreferrer">
+                            <img src={twitter} alt="Twitter" />
+                        </a>
+
+                        <a href="https://www.linkedin.com/company/yna-consult-group/" target="_blank" rel="noreferrer">
+                            <img src={linkedin} alt="LinkedIn" />
+                        </a>
+                    </div>
+                </div>
+            </footer>
+        </FooterStyle>
+    );
 };
 
 export default Footer;
