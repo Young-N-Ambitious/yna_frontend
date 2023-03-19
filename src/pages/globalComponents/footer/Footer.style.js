8cabe6e669a6f43823df51af731fe0bf80827027
Sydnee Sampson <sbsydnee161416@gmail.com>
2023-03-19 01:06:04 +0000

    Add landing pic to homepage and update footer styling/position

--- src/pages/globalComponents/footer/Footer.style.js
+++ src/pages/globalComponents/footer/Footer.style.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const FooterStyle = styled.div`
+    position: abosolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    height: 46px;
     .sectionContainer {
-        background-color: #ffd644;
-        position: absolute;
-        width: 100%;
-        bottom: 0;
-        height: 46px;
-    }
+            background-color: #ffd644;
+            position: relative;
+        }
     .footercontainer {
         font-weight: bold;
         height: 46px;
@@ -23,4 +25,10 @@ export const FooterStyle = styled.div`
     .linkspace {
         margin-right: 22px;
     }
+    @media screen and (min-height: 1000px) {
+        FooterStyle {
+          display: block;
+        }
+      }
 `;
+
---
+++ src/pages/landingpic/LandingPic.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LandingPicStyle = styled.div`
+    img {
+        width: 100%;
+        height: auto;
+        max-width: 100%;
+      }
+`;
