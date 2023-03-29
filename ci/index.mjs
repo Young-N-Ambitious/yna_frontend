8c629073739a07b224eb2a91bc6e5f0ca0bfdf5e
Mario Rojas Catalan <mariorojas.ac@gmail.com>
2023-03-29 00:45:40 -0400

    adding dagger cache

--- ci/index.mjs
+++ ci/index.mjs
@@ -2,39 +2,35 @@ import { connect } from "@dagger.io/dagger"
 
 
 connect(async (client) => {
+
+  // creating a cache volume 
+  const nodeCache = client.cacheVolume("node")
+
   // use a node:16-slim container
-  // mount the source code directory on the host
-  // at /src in the container
   const source = client.container()
     .from("node:16-slim")
     .withMountedDirectory('/src', client.host().directory('.', { exclude: ["node_modules/", "ci/"] }))
-
-  // set the working directory in the container
-  // install application dependencies
+    .withMountedCache("/src/node_modules", nodeCache)
+  // install application dependencies at working directory
   const runner = source
     .withWorkdir("/src")
     .withExec(["npm", "install"])
 
-  // run application tests
+  // run application tests : no test yet
 //   const test = runner
 //     .withExec(["npm", "test", "--", "--watchAll=false"])
 
-  // build application
-  // write the build output to the host
+  //build and write the build output to the host
   await runner
     .withExec(["npm", "run", "build"])
     .directory("./build")
     .export("./build")
 
-  // highlight-start
-  // use an nginx:alpine container
-  // copy the build/ directory into the container filesystem
-  // at the nginx server root
-  // publish the resulting container to a registry
+  // publish 
   const imageRef = await client.container()
     .from("nginx:1.23-alpine")
     .withDirectory('/usr/share/nginx/html', client.host().directory('./build'))
-    .publish('ttl.sh/yna-' + Math.floor(Math.random() * 10000000))
+    .publish('ttl.sh/yna-' + Math.floor(Math.random() * 10000000) + ':1d')
   console.log(`Published image to: ${imageRef}`)
   // highlight-end 
 
