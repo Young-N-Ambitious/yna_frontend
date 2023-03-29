9a4c9fb17c40b380c29081785b9d7b18d29e2f64
sydnee510 <sbsydnee161416@gmail.com>
2023-03-29 14:57:05 -0700

    Add who are we component to about us page

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "../globalComponents/navBar/NavBar";
 import { AboutStyle } from "./AboutUs.style.js";
-
+import WhoAreWe from "../whoAreWe/WhoAreWe";
 export default function AboutUs() {
     return (
         <AboutStyle>
             <div>
                 <NavBar />
-                <h1>About Us</h1>
+                <h3>About Us</h3>
+                <WhoAreWe></WhoAreWe>
             </div>
         </AboutStyle>
     );
---
+++ src/pages/whoAreWe/WhoAreWe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { WhoAreWeStyle } from "./WhoAreWe.style";
+
+export default function AboutUs() {
+    return (
+        <WhoAreWeStyle>
+            <div>
+                <h1>WhoAreWe</h1>
+                <p>We are a diverse group of tech enthusiasts who began coding together in the summer of 2019. We come from a wide range of backgrounds, but we all bring something special and unique to the Tech Industry, which sets us apart. While many of us are bootcamp grads, Computer Science students, and/or have come from other IT related fields, we each have something in common. We are Young And Ambitious and we attract like minded individuals who have the same goals, and plan to leave a mark on the entire tech force.</p>
+            </div>
+        </WhoAreWeStyle>
+    );
+}
