b67da59e19efdf3c2c4b75936829784dbca5c549
Sydnee Sampson <sbsydnee161416@gmail.com>
2023-04-12 19:20:28 +0000

    Render why us message on about us page

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "../globalComponents/navBar/NavBar";
 import { AboutStyle } from "./AboutUs.style.js";
-
+import WhyUs from "../aboutUS/whyUs/whyUs";
 export default function AboutUs() {
     return (
         <AboutStyle>
             <div>
                 <NavBar />
                 <h1>About Us</h1>
+                <WhyUs></WhyUs>
             </div>
         </AboutStyle>
     );
