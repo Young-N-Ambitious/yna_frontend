f77bddc1cfd47ecee5e5a1504e988e23ab4c5d5f
Mario Rojas Catalan <mariorojas.ac@gmail.com>
2023-05-10 23:30:06 -0400

    adding about component, adding styles and testing picture

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -3,12 +3,96 @@ import NavBar from "../globalComponents/navBar/NavBar";
 import { AboutStyle } from "./AboutUs.style.js";
 
 export default function AboutUs() {
+    const foundingMember = {
+        picture: "./assets/syd.jpg",
+        name: "Sydnee Sampsom",
+        title: "Founder",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+    };
+
+    const teamMembers = [
+        {   
+            picture: "",
+            name: "Danielle Madry",
+            title: "Founding Member",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+        },
+        {   
+            picture: "",
+            name: "LaNice Powell",
+            title: "Founding Member",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+        },
+        {   
+            picture: "",
+            name: "Diarte Jeffcoat",
+            title: "Founding Member",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+        },
+        {   
+            picture: "",
+            name: "Nicole Lucien",
+            title: "Founding Member",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+        },
+        {   
+            picture: "",
+            name: "Anika C",
+            title: "Founding Member",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+        },
+        {   
+            picture: "",
+            name: "Chaya Deaver",
+            title: "Founding Member",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+        },
+        {   
+            picture: "",
+            name: "Stefanie Davis",
+            title: "Founding Member",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque."
+        },
+        // Add more team members here
+    ];
+
     return (
         <AboutStyle>
-            <div>
-                <NavBar />
-                <h1>About Us</h1>
+        <div>
+            <NavBar />
+            <h1>About Us</h1>
+        </div>
+        <section>
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque.</p>
+        </section>
+        <section>
+            <h2>The Team</h2>
+            <div className="hero">
+                <img src={foundingMember.picture} alt={foundingMember.name} />
+                <h3>{foundingMember.name}</h3>
+                <h4>{foundingMember.title}</h4>
+                <p>{foundingMember.description}</p>
             </div>
-        </AboutStyle>
+        </section>
+        <section>
+            <h2>Our Team Members</h2>
+            <div className="team-members">
+                {teamMembers.map((member, index) => (
+                    <div key={index} className="member">
+                        <img src={member.picture} alt={member.name} />
+                        <h3>{member.name}</h3>
+                        <hr/>
+                        <h4>{member.title}</h4>
+                        <p>{member.description}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+        <section>
+            <h2>Why Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor nisi at dolor lacinia, ut finibus mauris cursus. Suspendisse a maximus neque.</p>
+        </section>
+    </AboutStyle>
     );
 }
--- src/pages/aboutUs/AboutUs.style.js
+++ src/pages/aboutUs/AboutUs.style.js
@@ -1,5 +1,81 @@
 import styled from "styled-components";
 
 export const AboutStyle = styled.div`
-    background-color: white;
+    background-color: #6A6863;
+    color: #FFFFFF;
+    section {
+        margin-bottom: 2rem;
+
+        h2 {
+            font-size: 1.5rem;
+            margin-bottom: 1rem;
+        }
+
+        .hero {
+            display: flex;
+            align-items: center;
+
+            img {
+                width: 150px;
+                height: 150px;
+                border-radius: 50%;
+                margin-right: 1rem;
+            }
+
+            h3 {
+                font-size: 1.2rem;
+                margin-bottom: 0.5rem;
+            }
+
+            h4 {
+                font-size: 1rem;
+                margin-bottom: 0.5rem;
+            }
+
+            p {
+                font-size: 1rem;
+            }
+        }
+        .team-members {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+            grid-gap: 1rem;
+            
+            .member {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                border: 2px solid #FFD644;
+                background-color: #000000;
+                
+                img {
+                    width: 150px;
+                    height: 150px;
+                    border-radius: 50%;
+                    margin-bottom: 1rem;
+                    border: 2px solid #FFD644;
+                }
+
+                hr {
+                    border: 2px solid #FFFFFF;
+                    width: 70%;
+                }
+                
+                h3 {
+                    font-size: 1.2rem;
+                    margin-bottom: 0.5rem;
+                }
+                
+                h4 {
+                    color: #FFD644;
+                    font-size: 1rem;
+                    margin-bottom: 0.5rem;
+                }
+
+                p {
+                    font-size: 1rem;
+                }
+            }
+        }
+    }
 `;
