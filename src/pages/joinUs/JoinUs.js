d87fc0217aa40d6ff5f4b92eb43e60693b20b4b2
Sincerely Brittany <sincerelybr@gmail.com>
2023-05-11 13:57:38 -0400

    adds embedded google docs to join us page - no styling yet

--- src/pages/joinUs/JoinUs.js
+++ src/pages/joinUs/JoinUs.js
@@ -3,14 +3,23 @@ import NavBar from "../globalComponents/navBar/NavBar";
 import { JoinStyle } from "./JoinUs.style.js";
 
 const JoinUs = () => {
-    return (
-        <JoinStyle>
-            <div>
-                <NavBar />
-                <h1>Hey!, You are Landed on Join Us, Congrats !!</h1>
-            </div>
-        </JoinStyle>
-    );
+  return (
+    <JoinStyle>
+      <div>
+        <NavBar />
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfEHpF9H3U9yCzmUPEiac637mECOzXAqrr9AXuxuz48KUd1pQ/viewform?embedded=true"
+          width="700"
+          height="520"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </JoinStyle>
+  );
 };
 
 export default JoinUs;
