09aaa0c63736fb6bf0eb91c7026616919cafcb6f
sydnee510 <sbsydnee161416@gmail.com>
2023-05-13 18:47:21 -0700

    Add styling back

--- src/pages/contactUs/ContactUs.style.js
+++ src/pages/contactUs/ContactUs.style.js
@@ -1,25 +1,25 @@
 import styled from "styled-components";
 
 export const ContactStyle = styled.div`
-font-family: 'Inter';
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-padding: 50px;
-background-color: #6A6863;
-color: #FFFFFF;   
-
-    .contact {
-        padding: 10px 30px;
-        background-color: gray;
-        // will change color once i update the icon colors
-        font-family: 'Inter';
-        h2 {
-            color: #FFC700;
-        }  
-        iframe {
-			display: block;
-			margin: auto;
-		}
-`;
+    background-color: white;font-family: 'Inter';
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 50px;
+    background-color: #6A6863;
+    color: #FFFFFF;   
+    
+        .contact {
+            padding: 10px 30px;
+            background-color: gray;
+            // will change color once i update the icon colors
+            font-family: 'Inter';
+            h2 {
+                color: #FFC700;
+            }  
+            iframe {
+                display: block;
+                margin: auto;
+            }
+    `;
