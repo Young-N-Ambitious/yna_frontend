e8525cc408c3e85ca456923169957a7f38c23c80
sydnee510 <sbsydnee161416@gmail.com>
2023-05-13 19:23:47 -0700

    Render yellow icons

--- src/pages/contactUs/ContactUs.js
+++ src/pages/contactUs/ContactUs.js
@@ -1,7 +1,8 @@
 import { ContactStyle } from "./ContactUs.style.js";
 import NavBar from "../globalComponents/navBar/NavBar";
-import twitter from "../../assets/twitter.png"
-import linkedin from "../../assets/Linkedin.png";
+import yellowTwitter from "../../assets/yellowTwitterIcon.png"
+import yellowLinkedin from "../../assets/yellowLinkedinIcon.png";
+import yellowGithub from "../../assets/yellowGithubIcon.png";
 
 function ContactUs() {
   return (
@@ -11,15 +12,9 @@ function ContactUs() {
     <div className="contact">
         <h1>Contact Us</h1>
         <h2>Get in touch with the @yna_squad today!</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <a href="https://www.linkedin.com/company/yna-consult-group" target="_blank" rel="noreferrer"> <img src={linkedin} alt="LinkedIn" />Linkedin</a> <br></br>
-                <a href="https://twitter.com/YNA_Squad" target="_blank" rel="noreferrer"> <img src={twitter} alt="Twitter" />Twitter</a> <br></br>
-                <a href="https://github.com/Young-N-Ambitious" target="_blank" rel="noreferrer">Github</a> <br></br>
+                <a href="https://www.linkedin.com/company/yna-consult-group" target="_blank" rel="noreferrer"> <img src={yellowLinkedin} alt="LinkedIn" /></a>
+                <a href="https://twitter.com/YNA_Squad" target="_blank" rel="noreferrer"> <img src={yellowTwitter} alt="Twitter" /></a>
+                <a href="https://github.com/Young-N-Ambitious" target="_blank" rel="noreferrer"> <img src={yellowGithub} alt="Github" /> </a> 
                 <iframe title="YNA Sponsor/Information Inquiry" src="https://docs.google.com/forms/d/e/1FAIpQLSdaVM6MOpbkvv1deoYmmJnxyLRd8rXnSiQRQoV8WKY_AAQmLQ/viewform?embedded=true" width="640" height="718" align="center" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>
         </ContactStyle>
