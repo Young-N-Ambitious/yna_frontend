4ee9af5b9915c27e398a28f06d152799fc6cd995
Sincerely Brittany <sincerelybr@gmail.com>
2023-05-15 10:11:06 -0400

    adds iframe to join us page

--- src/pages/joinUs/JoinUs.js
+++ src/pages/joinUs/JoinUs.js
@@ -1,25 +1,29 @@
 import React from "react";
 import NavBar from "../globalComponents/navBar/NavBar";
 import { JoinStyle } from "./JoinUs.style.js";
-import ContactPopUp from "../globalComponents/contactPopUp/ContactPopUp";
 
 const JoinUs = () => {
   return (
-    <JoinStyle>
-      <div>
-        <NavBar />
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSfEHpF9H3U9yCzmUPEiac637mECOzXAqrr9AXuxuz48KUd1pQ/viewform?embedded=true"
-          width="700"
-          height="520"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
-        >
-          Loading…
-        </iframe>
-      </div>
-    </JoinStyle>
+    <>
+      <NavBar />
+      <JoinStyle>
+        <div className="join-us">
+          <h1>Join Us</h1>
+          <iframe
+            class="responsive-iframe"
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfEHpF9H3U9yCzmUPEiac637mECOzXAqrr9AXuxuz48KUd1pQ/viewform?embedded=true"
+            // width="640"
+            // height="600"
+            align="center"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+          >
+            Loading…
+          </iframe>
+        </div>
+      </JoinStyle>
+    </>
   );
 };
 
--- src/pages/joinUs/JoinUs.style.js
+++ src/pages/joinUs/JoinUs.style.js
@@ -1,5 +1,38 @@
 import styled from "styled-components";
 
 export const JoinStyle = styled.div`
-    background-color: white;
+background-color: #6A6863;
+color: #FFFFFF;  
+height: 90vh; 
+
+    .join-us {
+        position: relative;
+        width: 100%;
+        height: 90%;
+        overflow: hidden;
+        h2 {
+            color: #FFC700;
+        }  
+        .responsive-iframe {
+            top: 0;
+            left: 0;
+            right: 0;
+            width: 100%;
+            height: 120%;
+            border: none;
+        } 
+        text-align: center;  
+        @media (max-width: 700px) {
+            .responsive-iframe{
+               position: absolute; 
+                top: 0;
+                left: 0;
+                bottom: 0;
+                right: 0;
+                width: 100%;
+                height: 100%;
+                border: none;
+            } 
+        }
+   
 `;
