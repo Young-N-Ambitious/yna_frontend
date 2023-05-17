d44751e2eb6a3ba7f5bb45c4c1da783938e34327
carlos luevano <clue@pop-os.localdomain>
2023-05-17 17:08:18 -0500

    fixed issue with iframe in joinus.js

--- src/pages/joinUs/JoinUs.js
+++ src/pages/joinUs/JoinUs.js
@@ -3,28 +3,27 @@ import NavBar from "../globalComponents/navBar/NavBar";
 import { JoinStyle } from "./JoinUs.style.js";
 
 const JoinUs = () => {
-  return (
-    <>
-      <NavBar />
-      <JoinStyle>
-        <div className="join-us">
-          <h1>Join Us</h1>
-          <iframe
-            class="responsive-iframe"
-            src="https://docs.google.com/forms/d/e/1FAIpQLSfEHpF9H3U9yCzmUPEiac637mECOzXAqrr9AXuxuz48KUd1pQ/viewform?embedded=true"
-            // width="640"
-            // height="600"
-            align="center"
-            frameborder="0"
-            marginheight="0"
-            marginwidth="0"
-          >
-            Loading…
-          </iframe>
-        </div>
-      </JoinStyle>
-    </>
-  );
+    return (
+        <>
+            <NavBar />
+            <JoinStyle>
+                <div className="join-us">
+                    <h1>Join Us</h1>
+                    <iframe
+                        title="join-us"
+                        class="responsive-iframe"
+                        src="https://docs.google.com/forms/d/e/1FAIpQLSfEHpF9H3U9yCzmUPEiac637mECOzXAqrr9AXuxuz48KUd1pQ/viewform?embedded=true"
+                        align="center"
+                        frameborder="0"
+                        marginheight="0"
+                        marginwidth="0"
+                    >
+                        Loading…
+                    </iframe>
+                </div>
+            </JoinStyle>
+        </>
+    );
 };
 
 export default JoinUs;
