bb8be9de0fc7604e46afe10413f9feb98b6c01fd
Sincerely Brittany <sincerelybr@gmail.com>
2023-05-17 20:53:48 -0400

    updates join us form styling

--- src/pages/joinUs/JoinUs.js
+++ src/pages/joinUs/JoinUs.js
@@ -3,27 +3,30 @@ import NavBar from "../globalComponents/navBar/NavBar";
 import { JoinStyle } from "./JoinUs.style.js";
 
 const JoinUs = () => {
-    return (
-        <>
-            <NavBar />
-            <JoinStyle>
-                <div className="join-us">
-                    <h1>Join Us</h1>
-                    <iframe
-                        title="join-us"
-                        class="responsive-iframe"
-                        src="https://docs.google.com/forms/d/e/1FAIpQLSfEHpF9H3U9yCzmUPEiac637mECOzXAqrr9AXuxuz48KUd1pQ/viewform?embedded=true"
-                        align="center"
-                        frameborder="0"
-                        marginheight="0"
-                        marginwidth="0"
-                    >
-                        Loading…
-                    </iframe>
-                </div>
-            </JoinStyle>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <JoinStyle>
+        <div className="join-us ">
+          <h1>Join Us</h1>
+          <h2>Join @yna_squad today!</h2>
+          <iframe
+            className="form"
+            title="YNA Sponsor/Information Inquiry"
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfEHpF9H3U9yCzmUPEiac637mECOzXAqrr9AXuxuz48KUd1pQ/viewform?embedded=true"
+            width="640"
+            height="718"
+            align="center"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+          >
+            Loading…
+          </iframe>
+        </div>
+      </JoinStyle>
+    </>
+  );
 };
 
 export default JoinUs;
