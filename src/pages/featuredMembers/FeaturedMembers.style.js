f216e8cb68212f019c57949005cdb9a1a71ee7dc
Sydnee Sampson <30539832+Sydnee510@users.noreply.github.com>
2023-05-19 22:25:16 -0700

    FD-002.3-v3-Create Featured Members Section (#100)
    * Add small social icons
    
    * Add featured members photos
    
    * Add featureed members component
    
    * Add featured members styling
    
    * Render featured members on home page

---
+++ src/pages/featuredMembers/FeaturedMembers.style.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+// export const FeaturedMembersStyle = styled.div`
+//   background-color: gray;
+// `;
+
+export const FeaturedContainer = styled.div``;
+
+export const FeaturedTitle = styled.h1`
+  text-align: center;
+  color: white;
+  padding-bottom: 40px;
+`;
+
+export const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 0px;
+  row-gap: 24px;
+  padding-left: 20px;
+  padding-bottom: 60px;
+  justify-items: center;
+`;
+
+export const Card1 = styled.div`
+  width: 470px;
+  height: 185px;
+  background-color: black;
+  border: 3px solid #ffd644;
+  display: flex;
+`;
+
+export const Card2 = styled.div`
+  width: 470px;
+  height: 185px;
+  background-color: black;
+  border: 3px solid #ffd644;
+  display: flex;
+`;
+
+export const Card3 = styled.div`
+  width: 470px;
+  height: 185px;
+  background-color: black;
+  border: 3px solid #ffd644;
+  display: flex;
+`;
+
+export const Card4 = styled.div`
+  width: 470px;
+  height: 185px;
+  background-color: black;
+  border: 3px solid #ffd644;
+  display: flex;
+`;
+
+export const Words = styled.div`
+  padding-top: 35;
+`;
+
+export const Icons = styled.div`
+  padding-top: 150px;
+`;
+
+export const Email = styled.p`
+  color: #ffffff;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+`;
+
+export const Name = styled.p`
+  color: #ffffff;
+  font-size: 29px;
+  font-weight: 700;
+  margin-bottom: -10px;
+`;
+
+export const JobTitle = styled.p`
+  color: #ffd644;
+  font-weight: 700;
+  font-size: 20px;
+  padding-top: 0;
+  padding-bottom: 20px;
+`;
+
+export const Icon1 = styled.img`
+  width: 121px;
+  height: 126px;
+  padding-top: 35px;
+  padding-left: 25px;
+  padding-bottom: 24px;
+  padding-right: 30px;
+  justify-content: space-between;
+`;
