a964c103fc88b837ec1c2c6d3ca227bacf70231b
Dorothy Gilchrist <dot.gilchrist95@gmail.com>
2023-07-17 01:49:03 +0000

    dropdown not needed for this page

--- src/pages/contactForm/ContactForm.js
+++ src/pages/contactForm/ContactForm.js
@@ -3,16 +3,8 @@ import NavBar from "../globalComponents/navBar/NavBar.js";
 import yellowTwitter from "../../assets/yellowTwitterIcon.png";
 import yellowLinkedin from "../../assets/yellowLinkedinIcon.png";
 import yellowGithub from "../../assets/yellowGithubIcon.png";
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
-
 
 function ContactForm() {
-
-    const [open, setOpen] = useState(false);
-
-
     return (
         <>
             <NavBar />
@@ -32,31 +24,9 @@ function ContactForm() {
                         {" "}
                         <img src={yellowGithub} alt="Github" />{" "}
                     </a>
-
-                    <div>
-
-                    </div>
-
-                    <Button
-                        onClick={() => setOpen(!open)}
-                        aria-controls="example-collapse-text"
-                        aria-expanded={open}
-                    >
-                        click
-                    </Button>
-                    <Collapse in={open}>
-                        <div id="example-collapse-text">
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-                            labore wes anderson cred nesciunt sapiente ea proident.
-                        </div>
-                    </Collapse>
-
-
+                    
                 </div>
             </ContactFormStyle>
-
-
         </>
     );
 }
