2e91cf2ce38630b5bec746aab0cacb303d930f1c
Candace <candace@Candaces-MacBook-Pro.local>
2023-09-27 11:07:22 -0400

    moved search to components, add search and filter logic with css

---
+++ src/components/Search/Search.js
@@ -0,0 +1,22 @@
+import { SearchStyle } from "./Search.style.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+const Search = ({ onSearch }) => {
+  return (
+    <SearchStyle>
+      <div className="search">
+        <input
+          className="searchInput"
+          placeholder="Search"
+          onChange={onSearch}
+        />
+        <div className="searchIcon">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
+      </div>
+    </SearchStyle>
+  );
+};
+
+export default Search;
